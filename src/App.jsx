ea6d9e36597df9db3bb53ccf348c211cce2f23a3
Khushbu K. <khushukat@gmail.com>
2025-05-18 18:39:00 +0530

    added the toggle btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
-
+import { CiCircleChevUp } from "react-icons/ci";
+import { CiCircleChevDown } from "react-icons/ci";
 function App() {
   const [color, setColor] = useState("olive");
+  const [visible, setVisible] = useState(false);
 
   const data = [
     { colorName: "Dark moderate pink", colorCode: "#a04a64" },
@@ -10,25 +12,43 @@ function App() {
     { colorName: "Strong yellow", colorCode: "#cdb332" },
   ];
 
+  const toggleVisibility = () => {
+    setVisible((prevState) => !prevState);
+    if (!visible) {
+      setTimeout(() => {
+        setVisible(false);
+      }, 5000);
+    }
+  };
+
   return (
     <>
       <div className="w-full h-screen" style={{ backgroundColor: color }}>
+        <div
+          onClick={toggleVisibility}
+          className="fixed flex justify-center bottom-36 md:bottom-24 inset-x-0 px-2 text-white"
+        >
+          {!visible ? (
+            <CiCircleChevUp className="size-10" />
+          ) : (
+            <CiCircleChevDown className="size-10" />
+          )}
+        </div>
         <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
-          <div className="flex flex-wrap justify-center gap-2 shadow-lg bg-white px-3 py-2 rounded-3xl">
-            {data.map((color, index) => (
-              <button
-                key={index}
-                className="outline-none px-4 py-1 rounded-full text-white shadow-lg cursor-pointer"
-                style={{ backgroundColor: color.colorCode }}
-                onClick={() => setColor(color.colorCode)}
-              >
-                {color.colorName}
-              </button>
-            ))}
-
-            {/* <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "green"}} onClick={()=> setColor("green")}>Green</button>
-            <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "blue"}} onClick={()=> setColor("blue")}>Blue</button> */}
-          </div>
+          {visible && (
+            <div className="flex flex-wrap justify-center gap-2 shadow-lg bg-white px-3 py-2 rounded-3xl">
+              {data.map((color, index) => (
+                <button
+                  key={index}
+                  className="outline-none px-4 py-1 rounded-full text-white shadow-lg cursor-pointer"
+                  style={{ backgroundColor: color.colorCode }}
+                  onClick={() => setColor(color.colorCode)}
+                >
+                  {color.colorName}
+                </button>
+              ))}
+            </div>
+          )}
         </div>
       </div>
     </>
